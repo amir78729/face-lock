3dddb82bc0c74ac167fa7d6d948c855fa4b1c29b
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2022-09-17 20:52:14 +0430

    change rectangles

--- utils/screen/faces.py
+++ utils/screen/faces.py
@@ -4,10 +4,18 @@ from utils.screen.texts import add_description_to_screen
 
 
 def draw_rectangle_on_screen(_frame, _y1, _x2, _y2, _x1, _color=(0, 200, 0), _text=''):
-    cv2.putText(_frame, _text, (_x1, _y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 6)
-    cv2.putText(_frame, _text, (_x1, _y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, _color, 2)
-    cv2.rectangle(_frame, (_x1, _y1), (_x2, _y2), (0, 0, 0), 6)
+    if _text == '':
+        cv2.rectangle(_frame, (_x1, _y1), (_x2, _y2), (0, 0, 0), 6)
+    else:
+        cv2.rectangle(_frame, (_x1, _y1 - 40), (_x2, _y2), (0, 0, 0), 6)
+        cv2.rectangle(_frame, (_x1, _y1 - 40), (_x2, _y1), (0, 0, 0), 6)
+
     cv2.rectangle(_frame, (_x1, _y1), (_x2, _y2), _color, 2)
+    if _text != '':
+        cv2.rectangle(_frame, (_x1, _y1 - 40), (_x2, _y1), (0, 0, 0), -1)
+        cv2.rectangle(_frame, (_x1, _y1 - 40), (_x2, _y1), _color, 2)
+        cv2.putText(_frame, _text, (_x1 + 10, _y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 6)
+        cv2.putText(_frame, _text, (_x1 + 10, _y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, _color, 2)
 
 
 def show_faces_on_screen(_frame):
