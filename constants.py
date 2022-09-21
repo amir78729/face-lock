8a40e803480d1c0369952c7752f3786194c7c891
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2022-09-21 20:05:25 +0430

    add get_char_from_numeric_sequence and get_numeric_sequence_from_char functions

--- constants.py
+++ constants.py
@@ -18,3 +18,41 @@ def get_configs(config_key):
     configs = json.load(config_file)
     config_file.close()
     return configs[config_key]
+
+
+mobile_numeric_keypad_dictionary = {
+    'a': '2',
+    'b': '22',
+    'c': '222',
+    'd': '3',
+    'e': '33',
+    'f': '333',
+    'g': '4',
+    'h': '44',
+    'i': '444',
+    'j': '5',
+    'k': '55',
+    'l': '555',
+    'm': '6',
+    'n': '66',
+    'o': '666',
+    'p': '7',
+    'q': '77',
+    'r': '777',
+    's': '7777',
+    't': '8',
+    'u': '88',
+    'v': '888',
+    'w': '9',
+    'x': '99',
+    'y': '999',
+    'z': '9999',
+}
+
+
+def get_char_from_numeric_sequence(_numeric_sequence):
+    return [k for k, v in mobile_numeric_keypad_dictionary.items() if v == _numeric_sequence][0]
+
+
+def get_numeric_sequence_from_char(_char):
+    return mobile_numeric_keypad_dictionary[_char]
