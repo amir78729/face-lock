93bcbecb445900aadd24e7be21b0f36a2305bf04
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-02-22 23:42:43 +0330

    master: 🔥 HOT FIX.

--- main_raspberry.py
+++ main_raspberry.py
@@ -26,9 +26,9 @@ if __name__ == '__main__':
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         # # grab the raw NumPy array representing the image, then initialize the timestamp
         # # and occupied/unoccupied text
-        # image = frame.array
-        # # show the frame
-        # cv2.imshow("Frame", image)
+        image = frame.array
+        # show the frame
+        cv2.imshow("Frame", image)
         # key = cv2.waitKey(1) & 0xFF
         # # clear the stream in preparation for the next frame
         # rawCapture.truncate(0)
