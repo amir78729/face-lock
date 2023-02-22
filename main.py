5a76a8e4f193ac20feb18a17d4e179b74adde99e
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-02-23 00:36:22 +0330

    master: Merge two main files.

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ from utils.user.add import add_user
 from utils.user.delete import delete_user
 from utils.user.authentication import enter_user
 from utils.files import get_configs
+import os
+
+is_raspberry = os.uname()[4][:3] == 'arm'
+
+if is_raspberry:
+    from picamera.array import PiRGBArray
+    from picamera import PiCamera
 
 if __name__ == '__main__':
-    # Encode faces.py from a folder
     fr = FaceRecognition()
     fr.load_encoding_images(get_configs('general')['images_path'])
 
-    # Load Camera
-    cap = cv2.VideoCapture(get_configs('general')['camera_arg'])
-    # CAMERA_IP = 'http://192.168.1.110:8080/video'
-
-    while True:
-        ret, frame = cap.read()
-        if not frame.any():
-            raise Exception('CAMERA NOT FOUND')
-
-        show_recognized_faces_on_screen(frame, fr)
-
+    def run(_frame, _raw_capture):
+        show_recognized_faces_on_screen(_frame, fr)
         key = cv2.waitKey(1)
+        # For Raspberry Pi Boards
+        if _raw_capture:
+            _raw_capture.truncate(0)
         if key == ENTER:
             enter_user(fr)
         if key == ord('a'):
@@ -32,7 +32,33 @@ if __name__ == '__main__':
         elif key == ord('d'):
             delete_user(fr)
         elif key == ESCAPE or key == ord('q'):
-            break
+            return False
+        return True
 
-    cap.release()
-    cv2.destroyAllWindows()
+
+    if is_raspberry:
+        camera = PiCamera()
+        camera.resolution = (640, 480)
+        camera.framerate = 32
+        raw_capture = PiRGBArray(camera, size=(640, 480))
+
+        for f in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+            frame = f.array
+            should_continue = run(frame, raw_capture)
+            if not should_continue:
+                break
+    else:
+        # Load Camera
+        cap = cv2.VideoCapture(get_configs('general')['camera_arg'])
+        # CAMERA_IP = 'http://192.168.1.110:8080/video'
+        while True:
+            ret, frame = cap.read()
+            if not frame.any():
+                raise Exception('CAMERA NOT FOUND')
+
+            should_continue = run(frame, None)
+            if not should_continue:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
