5cf947109a282e933d763efe9944bb1ffce72d55
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-02-24 19:43:57 +0330

    master: 🔥 HOT FIX.

--- utils/screen/capture.py
+++ utils/screen/capture.py
@@ -26,14 +26,24 @@ cap = cv2.VideoCapture(get_configs('general')['camera_arg'])  # TODO GLOBALIZE
 
 def capture_frame():
     if is_raspberry:
-        camera = PiCamera()
-        camera.resolution = RESOLUTION
-        camera.framerate = FRAME_RATE
-        time.sleep(2)
-        frame = np.empty((240 * 320 * 3,), dtype=np.uint8)
-        camera.capture(frame, 'bgr')
-        frame = frame.reshape((240, 320, 3))
-        return frame
+        with PiCamera() as camera:
+            camera.resolution = (100, 100)
+            camera.framerate = 24
+            time.sleep(2)
+            output = np.empty((112 * 128 * 3,), dtype=np.uint8)
+            camera.capture(output, 'rgb')
+            output = output.reshape((112, 128, 3))
+            output = output[:100, :100, :]
+            print(output)
+            return output
+        # camera = PiCamera()
+        # camera.resolution = RESOLUTION
+        # camera.framerate = FRAME_RATE
+        # time.sleep(2)
+        # frame = np.empty((240 * 320 * 3,), dtype=np.uint8)
+        # camera.capture(frame, 'bgr')
+        # frame = frame.reshape((240, 320, 3))
+        # return frame
     ret, frame = cap.read()
     if not frame.any():
         raise Exception('CAMERA NOT FOUND')
