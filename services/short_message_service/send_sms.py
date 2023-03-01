9859d9e3bbb0fb762b75576553a020dd2fd8a8f6
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-03-01 16:25:09 +0330

    master: 🔥 HOT FIX.

--- services/short_message_service/send_sms.py
+++ services/short_message_service/send_sms.py
@@ -2,31 +2,48 @@ import serial
 import time
 import RPi.GPIO as GPIO
 # from utils.log import log
-from utils.files import get_configs
+# from utils.files import get_configs
 
 GPIO.setmode(GPIO.BOARD)
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
 
 def send_sms(msg):
-    if get_configs('sms')['send_sms']:
-        port.write(b'AT\r')
-        rcv = port.read(10)
-        print(rcv)
-        time.sleep(1)
+    # if get_configs('sms')['send_sms']:
+    #     # port.write(b'AT\r')
+    #     # rcv = port.read(10)
+    #     # print(rcv)
+    #     # time.sleep(1)
+    #     #
+    #     # port.write(b"AT+CMGF=1\r")
+    #     #
+    #     # # log("Text Mode Enabled…")
+    #     # time.sleep(3)
+    #     # port.write(bytes('AT+CMGS="{}″\r'.format(get_configs('sms')['target_phone_number']), 'utf-8'))
+    #     # # log("sending message….")
+    #     # time.sleep(3)
+    #     # port.reset_output_buffer()
+    #     # time.sleep(1)
+    #     # port.write(str.encode(msg+chr(26)))
+    #     # time.sleep(3)
+    #     # # log("message sent...")
+    port.write(b'AT\r')
+    rcv = port.read(10)
+    print(rcv)
+    time.sleep(1)
 
-        port.write(b"AT+CMGF=1\r")
+    port.write(b"AT+CMGF=1\r")
 
-        # log("Text Mode Enabled…")
-        time.sleep(3)
-        port.write(bytes('AT+CMGS="{}″\r'.format(get_configs('sms')['target_phone_number']), 'utf-8'))
-        # log("sending message….")
-        time.sleep(3)
-        port.reset_output_buffer()
-        time.sleep(1)
-        port.write(str.encode(msg+chr(26)))
-        time.sleep(3)
-        # log("message sent...")
+    # log("Text Mode Enabled…")
+    time.sleep(3)
+    port.write(bytes('AT+CMGS="{}″\r'.format('989129334535'), 'utf-8'))
+    # log("sending message….")
+    time.sleep(3)
+    port.reset_output_buffer()
+    time.sleep(1)
+    port.write(str.encode(msg + chr(26)))
+    time.sleep(3)
+    # log("message sent...")
 
 
 send_sms('test')
