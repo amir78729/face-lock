0328ca179532d531a0631c6f1ee28a0b2c830042
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-01 19:00:42 +0330

    master: Handle keyboard interupt.

--- main.py
+++ main.py
@@ -16,44 +16,46 @@ from colorama import Style, Fore
 
 
 if __name__ == '__main__':
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(Fore.GREEN + CE_LOGO + Style.RESET_ALL)
-    print(Fore.GREEN + FACE_LOCK_SINGLE_LINE + Style.RESET_ALL)
-    fr = FaceRecognition()
-    fr.load_encoding_images(get_configs('general')['images_path'])
+    try:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(Fore.GREEN + CE_LOGO + Style.RESET_ALL)
+        print(Fore.GREEN + FACE_LOCK_SINGLE_LINE + Style.RESET_ALL)
+        fr = FaceRecognition()
+        fr.load_encoding_images(get_configs('general')['images_path'])
 
-    if is_raspberry:
-        frames, stream_capture = get_raspberry_frames()
-        for f in frames:
-            frame = f.array
-            add_time_to_screen(frame)
-            show_recognized_faces_on_screen(frame, fr)
-            key = cv2.waitKey(1)
-            key_keypad = read_keypad()
-            stream_capture.truncate(0)
-            if key == ENTER or key_keypad == KEYPAD_INPUTS['*']:
-                enter_user(fr)
-            if key == ord('a') or key == ord('A') or key_keypad == KEYPAD_INPUTS['A']:
-                add_user(fr)
-            elif key == ord('d') or key == ord('D') or key_keypad == KEYPAD_INPUTS['D']:
-                delete_user(fr)
-            elif key == ESCAPE or key == ord('q') or key == ord('Q') or key_keypad == KEYPAD_INPUTS['#']:
-                break
-    else:
-        while True:
-            cap = cv2.VideoCapture(get_configs('general')['camera_arg'])
-            ret, frame = cap.read()
-            add_time_to_screen(frame)
-            show_recognized_faces_on_screen(frame, fr)
-            key = cv2.waitKey(1)
-            if key == ENTER:
-                enter_user(fr)
-            if key == ord('a'):
-                add_user(fr)
-            elif key == ord('d'):
-                delete_user(fr)
-            elif key == ESCAPE or key == ord('q'):
-                break
-        cap.release()
-        cv2.destroyAllWindows()
-
+        if is_raspberry:
+            frames, stream_capture = get_raspberry_frames()
+            for f in frames:
+                frame = f.array
+                add_time_to_screen(frame)
+                show_recognized_faces_on_screen(frame, fr)
+                key = cv2.waitKey(1)
+                key_keypad = read_keypad()
+                stream_capture.truncate(0)
+                if key == ENTER or key_keypad == KEYPAD_INPUTS['*']:
+                    enter_user(fr)
+                if key == ord('a') or key == ord('A') or key_keypad == KEYPAD_INPUTS['A']:
+                    add_user(fr)
+                elif key == ord('d') or key == ord('D') or key_keypad == KEYPAD_INPUTS['D']:
+                    delete_user(fr)
+                elif key == ESCAPE or key == ord('q') or key == ord('Q') or key_keypad == KEYPAD_INPUTS['#']:
+                    break
+        else:
+            while True:
+                cap = cv2.VideoCapture(get_configs('general')['camera_arg'])
+                ret, frame = cap.read()
+                add_time_to_screen(frame)
+                show_recognized_faces_on_screen(frame, fr)
+                key = cv2.waitKey(1)
+                if key == ENTER:
+                    enter_user(fr)
+                if key == ord('a'):
+                    add_user(fr)
+                elif key == ord('d'):
+                    delete_user(fr)
+                elif key == ESCAPE or key == ord('q'):
+                    break
+            cap.release()
+            cv2.destroyAllWindows()
+    except KeyboardInterrupt:
+        print(Fore.RED + '\nExiting the Program...' + Style.RESET_ALL)
