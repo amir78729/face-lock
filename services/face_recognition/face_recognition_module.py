2e5a063512c8a313c9c6bb00bdfae7f61184ad3f
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-01 17:26:06 +0330

    master: Refactor.

--- services/face_recognition/face_recognition_module.py
+++ services/face_recognition/face_recognition_module.py
@@ -145,56 +145,34 @@ def batch_face_locations(images, number_of_times_to_upsample=1, batch_size=128):
     return list(map(convert_cnn_detections_to_css, raw_detections_batched))
 
 
-def _raw_face_landmarks(face_image, _face_locations=None, model='large'):
+def _raw_face_landmarks(face_image, _face_locations=None):
     if _face_locations is None:
         _face_locations = _raw_face_locations(face_image)
     else:
         _face_locations = [_css_to_rect(face_location) for face_location in _face_locations]
 
     pose_predictor = pose_predictor_68_point
-
-    if model == 'small':
-        pose_predictor = pose_predictor_5_point
-
     return [pose_predictor(face_image, face_location) for face_location in _face_locations]
 
 
-def face_landmarks(face_image, _face_locations=None, model='large'):
+def face_landmarks(face_image, _face_locations=None):
     """
     Given an image, returns a dict of face feature locations (eyes, nose, etc.) for each face in the image
 
     :param face_image: image to search
     :param _face_locations: Optionally provide a list of face locations to check.
-    :param model: Optional - which model to use. 'large' (default) or 'small' which only returns 5 points but is faster.
     :return: A list of dicts of face feature locations (eyes, nose, etc)
     """
-    landmarks = _raw_face_landmarks(face_image, _face_locations, model)
+    landmarks = _raw_face_landmarks(face_image, _face_locations)
     landmarks_as_tuples = [[(p.x, p.y) for p in landmark.parts()] for landmark in landmarks]
-    if model == 'large':
-        return [{
-            'chin': points[0:17],
-            'left_eyebrow': points[17:22],
-            'right_eyebrow': points[22:27],
-            'nose_bridge': points[27:31],
-            'nose_tip': points[31:36],
-            'left_eye': points[36:42],
-            'right_eye': points[42:48],
-            'top_lip': points[48:55] + [points[64]] + [points[63]] + [points[62]] + [points[61]] + [points[60]],
-            'bottom_lip':
-                points[54:60] + [points[48]] + [points[60]] + [points[67]] + [points[66]] + [points[65]] + [points[64]]
-        } for points in landmarks_as_tuples]
-    elif model == 'small':
-        return [{
-            'nose_tip': [points[4]],
-            'left_eye': points[2:4],
-            'right_eye': points[0:2],
-        } for points in landmarks_as_tuples]
-    else:
-        raise ValueError("Invalid landmarks model type. Supported models are ['small', 'large'].")
+    return [{
+        'nose_tip': [points[4]],
+        'left_eye': points[2:4],
+        'right_eye': points[0:2],
+    } for points in landmarks_as_tuples]
 
 
-# TODO: REMOVE num jitters
-def face_encodings(face_image, known_face_locations=None, num_jitters=1, model='small'):
+def face_encodings(face_image, known_face_locations=None, num_jitters=1):
     """
     Given an image, return the 128-dimension face encoding for each face in the image.
 
@@ -202,10 +180,9 @@ def face_encodings(face_image, known_face_locations=None, num_jitters=1, model='
     :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
     :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but
     slower (i.e. 100 is 100x slower)
-    :param model: Optional - which model to use. 'large' or 'small' (default) which only returns 5 points but is faster.
     :return: A list of 128-dimensional face encodings (one for each face in the image)
     """
-    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
+    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations)
     return [
         np.array(
             face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)
