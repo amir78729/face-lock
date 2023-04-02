a8f2f6857d6a939db7b1ddb19193651f45127961
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-03 01:23:05 +0330

    master: Add mtcnn.

--- services/face_recognition/face_recognition_module.py
+++ services/face_recognition/face_recognition_module.py
@@ -3,6 +3,11 @@ import dlib
 import numpy as np
 from PIL import ImageFile
 import face_recognition_models
+try:
+    from mtcnn.mtcnn import MTCNN
+    _face_detector = MTCNN()
+except:
+    pass
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -10,9 +15,6 @@ face_detector = dlib.get_frontal_face_detector()
 
 predictor_68_point_model = face_recognition_models.pose_predictor_model_location()
 pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model)
-
-predictor_5_point_model = face_recognition_models.pose_predictor_five_point_model_location()
-pose_predictor_5_point = dlib.shape_predictor(predictor_5_point_model)
 
 cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
 cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
