39917834cba0dedd9940ffe2d70f4ecac170113a
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-05 01:18:03 +0330

    master: 🔥 HOT FIX.

--- services/short_message_service/configure_sim900.py
+++ services/short_message_service/configure_sim900.py
@@ -10,7 +10,7 @@ port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1)
 # Transmitting AT Commands to the Modem
 # '\r\n' indicates the Enter key
 
-port.write('AT' + '\r\n')
+port.write('AT\r\n')
 rcv = port.read(10)
 print(rcv)
 time.sleep(1)
