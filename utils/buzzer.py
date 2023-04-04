2c73fa740639f34eab84a63521bbdb4a8f816b2e
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-04 22:31:48 +0330

    master: 🔥 HOT FIX.

--- utils/buzzer.py
+++ utils/buzzer.py
@@ -1,18 +1,16 @@
 import time
 from utils.system import is_raspberry
 
-buzzer = None
-
 BUZZER_DELAY = 0.3
 
 if is_raspberry:
     from gpiozero import Buzzer
-    buzzer = Buzzer(4)
 
 
 def buzz(n):
     if is_raspberry:
-        print('hi')
+        buzzer = Buzzer(4)
+        print(buzzer)
         for i in range(n):
             buzzer.on()
             time.sleep(BUZZER_DELAY)
