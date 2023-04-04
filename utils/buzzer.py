c1ed31a69bc47786ba6977d0a8d16bda19ac8642
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-05 00:18:31 +0330

    master: 🔥 HOT FIX.

--- utils/buzzer.py
+++ utils/buzzer.py
@@ -4,37 +4,31 @@ from utils.system import is_raspberry
 BUZZER_DELAY = 0.3
 BUZZER_PIN = 4
 
-theBuzzer = None
 if is_raspberry:
     from gpiozero import Buzzer
-    theBuzzer = Buzzer(BUZZER_PIN)
 
 
 def buzz(n):
-    global theBuzzer
     if is_raspberry:
-        # buzzer = Buzzer(BUZZER_PIN)
+        buzzer = Buzzer(BUZZER_PIN)
         for i in range(n):
-            # buzzer.on()
-            theBuzzer.on()
+            buzzer.on()
             time.sleep(BUZZER_DELAY)
-            # buzzer.off()
-            theBuzzer.off()
+            buzzer.off()
             time.sleep(BUZZER_DELAY)
 
 
 def buzz_on():
-    global theBuzzer
     if is_raspberry:
-        # buzzer = Buzzer(BUZZER_PIN)
-        # buzzer.on()
-        theBuzzer.on()
+        buzzer = Buzzer(BUZZER_PIN)
+        buzzer.on()
+        time.sleep(BUZZER_DELAY)
 
 
 def buzz_off():
-    global theBuzzer
     if is_raspberry:
-        # buzzer = Buzzer(BUZZER_PIN)
-        # buzzer.off()
-        theBuzzer.off()
+        buzzer = Buzzer(BUZZER_PIN)
+        buzzer.off()
+        time.sleep(BUZZER_DELAY)
+
 
