0f236cefd98dcc1ab1bdb8b3341ad0cdee4bcd0f
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-05 01:28:42 +0330

    master: Update sim900 module.

--- services/short_message_service/configure_sim900.py
+++ services/short_message_service/configure_sim900.py
@@ -1,43 +1,51 @@
-from utils.system import is_raspberry
+import serial
+import RPi.GPIO as GPIO
+import time
 
-if is_raspberry:
-    import serial
-    import time
-    import RPi.GPIO as GPIO
-    GPIO.setmode(GPIO.BOARD)
-    port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
+GPIO.setmode(GPIO.BOARD)
 
-    port.write(b'AT\r')
+# Enable Serial Communication
+port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
+
+# Transmitting AT Commands to the Modem
+# '\r\n' indicates the Enter key
+
+port.write(b'AT\r\n')
+rcv = port.read(10)
+print(rcv)
+time.sleep(1)
+
+port.write(b'ATE0\r\n')  # Disable the Echo
+rcv = port.read(10)
+print(rcv)
+time.sleep(1)
+
+port.write(b'AT+CMGF=1\r\n')  # Select Message format as Text mode 
+rcv = port.read(10)
+
+print(rcv)
+time.sleep(1)
+
+port.write(b'AT+CNMI=2,1,0,0,0\r\n')  # New SMS Message Indications
+rcv = port.read(10)
+
+print(rcv)
+time.sleep(1)
+
+# Sending a message to a particular Number
+
+port.write(b'AT+CMGS="989129334535"\r\n')
+rcv = port.read(10)
+
+print(rcv)
+time.sleep(1)
+
+port.write(b'Hello User\r\n')  # Message
+rcv = port.read(10)
+
+print(rcv)
+
+port.write("\x1A")  # Enable to send SMS
+for i in range(10):
     rcv = port.read(10)
     print(rcv)
-    time.sleep(1)
-
-    port.write(b"ATE0\r")
-    rcv = port.read(10)
-    print(rcv)
-    time.sleep(1)
-
-    port.write(b'AT+CNMI=1,1,0,0,0\r')
-    rcv = port.read(30)
-    print(rcv)
-    time.sleep(1)
-
-    port.write(b'AT+CPMS="SM","SM","SM"\r')
-    rcv = port.read(30)
-    print(rcv)
-    time.sleep(1)
-
-    port.write(b'AT+CSAS\r')
-    rcv = port.read(30)
-    print(rcv)
-    time.sleep(10)
-
-    port.write(b'AT+CMGDA="DEL ALL"\r')
-    rcv = port.read(30)
-    print(rcv)
-    time.sleep(1)
-
-    port.write(b'AT+CPMS="SM","SM","SM"\r')
-    rcv = port.read(30)
-    print(rcv)
-    time.sleep(1)
