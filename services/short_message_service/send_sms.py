c90d70aeee10fedc92593bf00b028b4ce4956547
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-09 18:21:59 +0330

    master: 🔥 HOT FIX.

--- services/short_message_service/send_sms.py
+++ services/short_message_service/send_sms.py
@@ -69,16 +69,16 @@ SERIAL_PORT = "/dev/ttyS0"    # Raspberry Pi 3
 
 ser = serial.Serial(SERIAL_PORT, baudrate=9600, timeout=5)
 setup()
-ser.write("AT+CMGF=1\r")  # set to text mode
+ser.write(b"AT+CMGF=1\r")  # set to text mode
 time.sleep(3)
-ser.write('AT+CMGDA="DEL ALL"\r')  # delete all SMS
+ser.write(b'AT+CMGDA="DEL ALL"\r')  # delete all SMS
 time.sleep(3)
 reply = ser.read(ser.inWaiting())  # Clean buf
 print("Listening for incomming SMS...")
 while True:
     reply = ser.read(ser.inWaiting())
     if reply != "":
-        ser.write("AT+CMGR=1\r")
+        ser.write(b"AT+CMGR=1\r")
         time.sleep(3)
         reply = ser.read(ser.inWaiting())
         print("SMS received. Content:")
@@ -89,13 +89,13 @@ while True:
                 state = "Button released"
             else:
                 state = "Button pressed"
-            ser.write('AT+CMGS="+989129334535"\r')
+            ser.write(b'AT+CMGS="+989129334535"\r')
             time.sleep(3)
             msg = "Sending status at " + t + ":--" + state
             print("Sending SMS with status info:" + msg)
             ser.write(msg + chr(26))
         time.sleep(3)
-        ser.write('AT+CMGDA="DEL ALL"\r')  # delete all
+        ser.write(b'AT+CMGDA="DEL ALL"\r')  # delete all
         time.sleep(3)
         ser.read(ser.inWaiting())  # Clear buf
     time.sleep(5)
