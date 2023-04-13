f28777f0c2760ac8dc553faa807ea05d520789a3
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-13 15:05:54 +0330

    master: Add test for lock.

--- test/lock.py
+++ test/lock.py
@@ -2,6 +2,7 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.OUT)
+GPIO.setup(2, GPIO.OUT)
 
 if __name__ == '__main__':
     lock = 0
@@ -10,9 +11,11 @@ if __name__ == '__main__':
             lock = 1
             print('locking door...')
             GPIO.output(24, lock)
+            GPIO.output(2, lock)
             time.sleep(1)
         else:
             lock = 0
             print('unlocking door...')
             GPIO.output(24, lock)
+            GPIO.output(2, lock)
             time.sleep(1)
