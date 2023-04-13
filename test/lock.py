013a0f0811c8e2675077526331a6943124385720
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-13 13:49:22 +0330

    master: Add test for lock.

---
+++ test/lock.py
@@ -0,0 +1,19 @@
+# from constants.raspberry_pi import LOCK_PIN
+import time
+# import RPi.GPIO as GPIO
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(LOCK_PIN, GPIO.OUT)
+
+if __name__ == '__main__':
+    lock = 0
+    while True:
+        if lock == 0:
+            lock = 1
+            print('locking door...')
+            # GPIO.output(24, lock)
+            time.sleep(1)
+        else:
+            lock = 0
+            print('unlocking door...')
+            # GPIO.output(24, lock)
+            time.sleep(1)
