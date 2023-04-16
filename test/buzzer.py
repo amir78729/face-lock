7457e3ffe6f7e32deaca8f3891eb50194a95a1c7
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-16 18:45:46 +0330

    master: Update tests.

--- test/buzzer.py
+++ test/buzzer.py
@@ -1,14 +1,14 @@
 import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 
 if __name__ == '__main__':
     while True:
         print('locking door...')
-        GPIO.output(4, 0)
+        GPIO.output(17, 0)
         time.sleep(1)
 
         print('unlocking door...')
-        GPIO.output(4, 1)
+        GPIO.output(17, 1)
         time.sleep(1)
